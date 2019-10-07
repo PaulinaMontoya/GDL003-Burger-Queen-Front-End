676f53e4bd6eef8e3c174509c6c38bad9b6d0d7d
JoannaArechiga <issassam95@hotmail.com>
2019-10-07 18:38:28 -0500

    Updates

---
+++ precache-manifest.b63cacb963fe03fb0e0e65fc36f55c88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05db071c6c3caadde20a8b55d69701a5",
+    "url": "/burger-kawaii/index.html"
+  },
+  {
+    "revision": "f51a719206eb86ddfa5c",
+    "url": "/burger-kawaii/static/css/main.51d50eb0.chunk.css"
+  },
+  {
+    "revision": "afc75ce6ce135714f423",
+    "url": "/burger-kawaii/static/js/2.6a394ff8.chunk.js"
+  },
+  {
+    "revision": "f51a719206eb86ddfa5c",
+    "url": "/burger-kawaii/static/js/main.2e3ed67c.chunk.js"
+  },
+  {
+    "revision": "f3d2ebb4f9caeb053c18",
+    "url": "/burger-kawaii/static/js/runtime-main.d897bf2e.js"
+  },
+  {
+    "revision": "20e74f9578bef02e834aaa60536410f9",
+    "url": "/burger-kawaii/static/media/breackfast-choose.20e74f95.png"
+  },
+  {
+    "revision": "7b06742c90463aeb00e4525952ffd410",
+    "url": "/burger-kawaii/static/media/button-breakfast.7b06742c.png"
+  },
+  {
+    "revision": "b14b1e7bfa98d35ef3590ce1dc911890",
+    "url": "/burger-kawaii/static/media/button-dinner.b14b1e7b.png"
+  },
+  {
+    "revision": "00fea45a40fb720aa59d39209d1a8e77",
+    "url": "/burger-kawaii/static/media/button-enter.00fea45a.png"
+  },
+  {
+    "revision": "2ded62d0eb482f63677c4a9ef6bd29be",
+    "url": "/burger-kawaii/static/media/button-sendtokitchen.2ded62d0.png"
+  },
+  {
+    "revision": "fa05feff030e57d8c4e05a0a0a180457",
+    "url": "/burger-kawaii/static/media/button-waitress.fa05feff.png"
+  },
+  {
+    "revision": "8cfb6f157775a8918636449ff4d31ae5",
+    "url": "/burger-kawaii/static/media/circle.8cfb6f15.png"
+  },
+  {
+    "revision": "5959d94bbf02125f8340381425038c73",
+    "url": "/burger-kawaii/static/media/dinner-choose.5959d94b.png"
+  }
+]);
