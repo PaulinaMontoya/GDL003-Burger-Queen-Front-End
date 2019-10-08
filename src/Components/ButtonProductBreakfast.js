7f94220a456bbfbd3723ddfa1e13644f2262eaa9
anndy9192 <andybmq@gmail.com>
2019-10-08 16:35:18 -0500

    Fetch and token for menu breakfast

--- src/Components/ButtonProductBreakfast.js
+++ src/Components/ButtonProductBreakfast.js
@@ -6,22 +6,46 @@ import menu from "../menuData.js";
 
 class ButtonProductBreakfast extends Component {
 
-  render() {
-  
-    return (
+   /* var cors = require('cors')
+    
+    app.route('/menuBreakfast')
+    //GET endpoint for menuBreakfast
+    .get(getMenuBreakfast, cors());
 
-      <div >
-        {menu.breakfast.map((item) =>
-          <div className="cointenerButtonProduct" key={item.id}>
-            <img src={item.img} alt={item.dish} className="imgButtonProduct"></img>
-            <p className="nameButtonProduct">{item.dish}</p>
-            <p className="nameButtonProduct">${item.price}</p>
-            <button className="buttonPlusProduct" onClick={() => this.props.getValuesMethod(item.id)}>+</button>
-          </div>
-        )}
-      </div>
-    )
-  }
+    app.route('/menuBreakfast', cors())*/
+
+
+    componentDidMount() {
+        const headers = new Headers();
+        headers.set("token", "pM170290aM291287mR270983dP160591");
+        //https://api.github.com/orgs/nodejs
+        fetch('http://172.17.33.47/menuBreakfast', {
+            method: "GET",
+            headers: headers,
+          })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data)
+          })
+          .catch(error => console.error(error))
+    }
+
+    render() {
+
+        return (
+
+            <div >
+                {menu.breakfast.map((item) =>
+                    <div className="cointenerButtonProduct" key={item.id}>
+                        <img src={item.img} alt={item.dish} className="imgButtonProduct"></img>
+                        <p className="nameButtonProduct">{item.dish}</p>
+                        <p className="nameButtonProduct">${item.price}</p>
+                        <button className="buttonPlusProduct" onClick={() => this.props.getValuesMethod(item.id)}>+</button>
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
 
 export default ButtonProductBreakfast;
